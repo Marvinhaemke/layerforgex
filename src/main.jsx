a14e179958d5b400acf765d69f346eb631aba897
Marvinhaemke <haemke@th-brandenburg.de>
2026-04-28 19:43:27 +0200

    alt index

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import Impressum from './Impressum.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        {/* Die Hauptseite (unsere lange Landingpage) */}
+        <Route path="/" element={<App />} />
+        
+        {/* Die neue Unterseite */}
+        <Route path="/index2" element={<Index2 />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
